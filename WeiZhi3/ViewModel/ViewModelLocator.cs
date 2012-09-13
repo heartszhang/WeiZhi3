c8c085420415d1f2ebb9b64bdecb4c2ce4c1c840
hearts <hearts.zhang@gmail.com>
2012-09-13 08:52:26 +0800

    统一xaml文件uielement名称，使用成员变量_all_lowerer模式 增加ViewModelLocator支持
    Signed-off-by: hearts <hearts.zhang@gmail.com>

---
+++ WeiZhi3/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeiZhi3.ViewModel
+{
+    class ViewModelLocator
+    {
+    }
+}
