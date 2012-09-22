c01944eb34e51c06b8f326ac7f1744d50726fbc4
hearts <hearts.zhang@gmail.com>
2012-09-22 16:54:37 +0800

    整理代码
    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- WeiZhi3/Converters/WindowMaximzedToBooleanConverter.cs
+++ WeiZhi3/Converters/WindowMaximzedToBooleanConverter.cs
@@ -27,21 +27,4 @@ namespace WeiZhi3.Converters
 
         #endregion
     }
-
-    [ValueConversion(typeof(string), typeof(Visibility))]
-    public class ImageUrlConverter : IValueConverter
-    {
-        // Methods
-        public object Convert(object value, Type target_type, object parameter, System.Globalization.CultureInfo culture)
-        {
-            var url = (string) value;
-            return string.IsNullOrEmpty(url) ? null : new Uri(url);
-        }
-
-        public object ConvertBack(object value, Type target_type, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
 }
